5e2aad1c1aca87437f0672ee41db51b76261fbb2
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-04-06 21:36:33 -0300

    Create execute.sh

---
+++ execute.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+principal() {
+echo
+echo "[1] Search Public IPs"
+echo "[2] Locate IPs"
+echo "[3] Portscan IPs"
+echo "[4] Exit"
+read -p "Your choice: " CHOICE
+
+case $CHOICE in
+  1) search;;
+  2) locate;;
+  3) portscan;;
+  4) exit;;
+  *) sleep 0.5 && principal;;
+esac
+}
+
+search () {
+
+}
+
+locate () {
+
+}
+
+portscan () {
+
+}
+
+exit()
+{
+   exit
+}
+principal
