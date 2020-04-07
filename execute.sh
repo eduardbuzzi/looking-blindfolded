0209da04b49585b5ba701afb04d6a2363095446a
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-04-06 22:17:38 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -5,6 +5,7 @@ echo "[1] Search Public IPs"
 echo "[2] Locate IPs"
 echo "[3] Portscan IPs"
 echo "[4] Exit"
+echo
 read -p "Your choice: " CHOICE
 
 case $CHOICE in
@@ -17,7 +18,25 @@ esac
 }
 
 search () {
-echo 
+echo
+read -p "How many random IPs do you want to check if are online? " FINAL
+read -p "Name of the list that will receive the IPs that are Online: " LISTNAME
+echo
+for i in `seq 1 $FINAL`
+do
+NUM1=$(shuf -i 1-217 -n1)
+NUM2=$(shuf -i 1-254 -n1)
+NUM3=$(shuf -i 1-254 -n1)
+NUM4=$(shuf -i 0-254 -n1) 
+HOST="$NUM1.$NUM2.$NUM3.$NUM4"    
+echo "Checking if the IP $HOST is Online.."
+PING=$(ping -c 1 $HOST | grep "64 bytes" | cut -d ' ' -f4 | sed 's/.$//')
+echo $PING >> .$LISTNAME
+done
+awk 'NF>0' .$LISTNAME >> $LISTNAME
+rm .$LISTNAME 2> /dev/null
+echo
+principal
 }
 
 locate () {
